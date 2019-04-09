9e8b1249a671aca5a345fc52c8993abf29f91340
Sergey Vedmedev <vedmedev@trueconf.ru>
2019-04-09 23:14:52 +0300

    Intro done!

--- index.js
+++ index.js
@@ -4,3 +4,64 @@ var app = new Vue ({
     message: 'Hello World!'
   }
 })
+
+var app2 = new Vue ({
+  el: '#app-2',
+  data: {
+    message: 'Page is loaded: ' + new Date().toLocaleString()
+  },
+})
+
+var app3 = new Vue ({
+  el: '#app-3',
+  data: {
+    seen: true
+  }
+})
+
+var app4 = new Vue ({
+  el: '#app-4',
+  data: {
+    todos: [
+      {text: 'Learn JavaScript'},
+      {text: 'Lern Vue'},
+      {text: 'Freelance on Vue to buy laptop'},
+      {text: 'Goodbye Trueconf'},
+      {text: 'Write last letter about company fails and headquaters'}
+    ]
+  }
+})
+
+var app5 = new Vue({
+  el: '#app-5',
+  data: {
+    message: 'Hello, Vue.js!'
+  },
+  methods: {
+    reverseMessage: function () {
+      this.message = this.message.split('').reverse().join('')    }
+  }
+})
+
+var app6 = new Vue({
+  el: '#app-6',
+  data: {
+    message: 'Hello, Vue!'
+  }
+})
+
+Vue.component('todo-item', {
+  props: ['todo'],
+  template: '<li>{{ todo.text }}</li>'
+})
+
+var app7 = new Vue({
+  el: '#app-7',
+  data: {
+    groceryList: [
+      { id: 0, text: 'Vegetables' },
+      { id: 1, text: 'Cheese' },
+      { id: 2, text: 'Whatever else' }
+    ]
+  }
+})
